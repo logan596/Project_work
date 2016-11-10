13cc51c0686f17967bffd4f80aa15bd5af639e19
root <root@linuxgeeks.in>
2016-11-09 23:01:30 -0500

    link added for sshd service

--- starttest.sh
+++ starttest.sh
@@ -8,10 +8,17 @@
 
 #First writing Algorithm.
 
-http://unix.stackexchange.com/questions/109559/shell-script-to-wait-for-a-packet-from-a-certain-port
+#http://unix.stackexchange.com/questions/109559/shell-script-to-wait-for-a-packet-from-a-certain-port
 
-https://azouhr.wordpress.com/2012/06/21/port-forwarding-with-xinetd/
+#https://azouhr.wordpress.com/2012/06/21/port-forwarding-with-xinetd/
 
-https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Security_Guide/sect-Security_Guide-TCP_Wrappers_and_xinetd-xinetd_Configuration_Files.html
+#https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Security_Guide/sect-Security_Guide-TCP_Wrappers_and_xinetd-xinetd_Configuration_Files.html
 
-http://www.cyberciti.biz/faq/linux-how-do-i-configure-xinetd-service/
+#http://www.cyberciti.biz/faq/linux-how-do-i-configure-xinetd-service/
+
+docker start sad_shannon
+docker exec -it sad_shannon bash
+
+Follow the link
+
+https://docs.docker.com/engine/examples/running_ssh_service/
