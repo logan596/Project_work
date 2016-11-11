4b8d68d7a0943a5501af586c2a097642f0df2866
Yogesh Gavhane <yogeshgavhane596@gmail.com>
2016-11-11 03:06:04 -0500

    port forward add

---
+++ starttest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#This script is belogs to linuxgeeks.in 
+#For create a new server every time user is started the test.
+
+
+#Nothing is impossible, i ca try this..
+
+#First writing Algorithm.
+
+http://unix.stackexchange.com/questions/109559/shell-script-to-wait-for-a-packet-from-a-certain-port
+
+https://azouhr.wordpress.com/2012/06/21/port-forwarding-with-xinetd/
+
+https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Security_Guide/sect-Security_Guide-TCP_Wrappers_and_xinetd-xinetd_Configuration_Files.html
+
+
