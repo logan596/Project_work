68ba089296697cb91249527c578ddd43b6031786
Yogesh Gavhane <yogeshgavhane596@gmail.com>
2016-12-10 20:07:31 +0530

    Question 3 added

---
+++ question3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##########################################################################################     
+# Create a directory named /common. Allow john and davis to share documents in the       #
+#/common directory using a group called team. Both of them can read, write and remove    #
+# documents from the other in this  directory but any user not member of the group can’t.#                                                
+##########################################################################################
+
+
+SETPERM=$(getfacl -p /common | grep team | awk -F : '{print $3}')
+
+if [ ${SETPERM} == "rwx" ]
+then
+	echo "Permission is CORRECT"
+	exit 0
+else
+	exit 1
+fi
+
+
