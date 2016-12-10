05bd64bef268a8a8546dddb8ee1da5a3c9db8d63
Yogesh Gavhane <yogeshgavhane596@gmail.com>
2016-12-10 19:11:23 +0530

    Check user davis account info

--- passwordcheck2.sh
+++ passwordcheck2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+EXP=`date -d "30 days" +"%b-%d-%Y"`
+ACC_EXP=`chage -l davis | grep ^Account | awk -F : '{print $2}' | sed 's/\ //' | sed 's/\,//g' |sed 's/\ /-/g'`
 
-read USERNAME <<< "john"
-
+read USERNAME <<< "davis"
+que1_1=$(cat /etc/passwd | grep  ^davis | awk -F : '{print $3}')
 id -u $USERNAME > /dev/null
 if [ $? -ne 0 ]
 then
@@ -15,7 +17,7 @@ else
         export ALGO=`echo $ORIGPASS | cut -d'$' -f2`
         export SALT=`echo $ORIGPASS | cut -d'$' -f3`
         GENPASS=$(perl -le 'print crypt("$ENV{PASSWD}","\$$ENV{ALGO}\$$ENV{SALT}\$")')
-        if [ "$GENPASS" == "$ORIGPASS" ]
+        if [ "$GENPASS" == "$ORIGPASS" ] && [ "$que1_1" -eq "3000" ]  && [ "${EXP}" == "${ACC_EXP}" ]
         then
                 echo "Valid Username-Password Combination"
                 exit 0
@@ -24,5 +26,4 @@ else
                 exit 1
         fi
 fi
-~
 
