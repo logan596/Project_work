d0c8a715077787dadbcb62405d2aedc41f206202
Yogesh Gavhane <yogeshgavhane596@gmail.com>
2016-12-15 07:52:33 +0530

    Question 5 added

---
+++ scripts/question5.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+###################################################################################
+#   Setup a /home/rhce directory to facilitate collaboration among the rhce group.#
+#   Each member should be able to create files and modify each others’ files,     #
+#   but should not be able to delete any one else’s files in this directory.      #
+###################################################################################
+
+FIRST_CHECK=$(getfacl -p /rhce/ | grep group  | head -1 | awk '{print $3}')
+SECOND_CHECK=$(getfacl -p /rhce/ | grep group | tail -1 | awk -F "::" '{print $2}')
+
+#First check for group exeist or not
+if [ ${FIRST_CHECK} == "redhat" -a  ${SECOND_CHECK} == "rwx" ]
+
+then
+       	echo "Permission is CORRECT"
+       	exit 0
+   
+     
+else
+        exit 1
+fi
+
